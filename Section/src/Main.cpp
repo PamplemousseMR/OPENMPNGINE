e6aa1c7b4888e2ee3f015b1a8cd143db4133dd81
romain <romain.manciaux@gmail.com>
2018-09-10 13:44:43 +0200

    feat(Section): add reduction exemple

--- Section/src/Main.cpp
+++ Section/src/Main.cpp
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <vector>
 #include <stdlib.h>
+
 /*
     this project test opm section
 */
@@ -40,6 +41,33 @@ int main()
         {
             assert(arr[static_cast< std::vector< int >::size_type >(i)] == arr[static_cast< std::vector< int >::size_type >(max-1)]);
         }
+    }
+    //====================
+    //  Reduction
+    //====================
+    {
+        int max = omp_get_max_threads();
+        int res = 0;
+
+        #pragma omp parallel sections reduction(+:res)
+        {
+            #pragma omp section
+            {
+                for(int i=0 ; i<max/2 ; ++i)
+                {
+                    ++res;
+                }
+            }
+            #pragma omp section
+            {
+                for(int i=max/2 ; i<max ; ++i)
+                {
+                    ++res;
+                }
+            }
+        }
+
+        assert(res == max);
     }
 
     return EXIT_SUCCESS;
